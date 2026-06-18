7f013c69b9cafcfc109c0fd55bb427129b19cd8c
Yagogo39 <carlosmarcialdiego@gmail.com>
2026-06-17 23:18:28 -0600

    Arreglos del Duende

--- routes/notas_route.php
+++ routes/notas_route.php
@@ -4,6 +4,11 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 include_once __DIR__ . '/../config/db_connection.php';
 include_once __DIR__ . '/../controllers/notas_controller.php';
 
--- routes/usuario_route.php
+++ routes/usuario_route.php
@@ -5,6 +5,11 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 include_once __DIR__ . '/../config/db_connection.php';
 include_once __DIR__ . '/../controllers/usuario_controller.php';
 
